471c12af1022bd778da8c1b385ae64d0b8eb9ba9
mferfarias <fernanda.farias.u.u@gmail.com>
2021-11-11 01:36:08 -0300

    fer logró hacer funcional el registro de usuarios

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,14 +1,45 @@
+const fs = require('fs');
 const path = require('path');
+const bcrypt = require("bcryptjs")
+
+const usersFilepath = path.join(__dirname, '../data/usersDatabase.json');
+const users = JSON.parse(fs.readFileSync(usersFilepath, 'utf-8'));
+
 
 const controller = {
-    login: (req, res)=>{
-    let pathLogin = path.join(__dirname, '../views/users/login.ejs')
-       res.render(pathLogin) 
-    },
-    registro: (req, res)=>{
-        let pathregistro = path.join(__dirname, '../views/users/registro.ejs')
-           res.render(pathregistro) 
-        }
+   login: (req, res)=>{
+      let pathLogin = path.join(__dirname, '../views/users/login.ejs')
+      res.render(pathLogin) 
+   },
+   registro: (req, res)=>{
+      let pathregistro = path.join(__dirname, '../views/users/registro.ejs')
+      res.render(pathregistro)
+   },
+
+   createUser: (req,res) => {
+      let newUser = {
+         nombre: req.body.nombre,
+         apellido: req.body.apellido,
+         usuario: req.body.usuario,
+         email: req.body.email,
+         fechaNacimiento: req.body.fechaNacimiento,
+         contraseña: bcrypt.hashSync(req.body.contraseña,10),
+      };
+      console.log(newUser);
+      users.push(newUser);
+      console.log('User creado');
+      let usersJson = JSON.stringify(users);
+      fs.writeFileSync(usersFilepath, usersJson);
+      console.log('User cargado al jsonDataBase :)');
+      res.redirect("/")
+
+   },
+   userProfile: (req, res) => {
+      let userId = req.params.id;
+      let user = users.find(user => user.id == userId);
+      let pathViewDetail = path.join(__dirname, '../views/users/userProfile.ejs');
+      res.render(pathViewDetail, {user})
+   },
 };
 
 module.exports=controller;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
-
+const multer = require("multer");
 const userController = require('../controllers/usersController');
+const path = require ("path")
+
+const diskStorage = multer.diskStorage ({
+    destination: (req, file,cb) =>{
+        cb (null, path.join(__dirname,"../../public/images/users"));
+    },
+    filename: (req, file, cb) => {
+        const newFilename = "user-" + Date.now() + path.extname(file.originalname);
+        cb (null, newFilename);
+    }
+})
+
+const upload = multer ({storage: diskStorage})
 
 router.get('/login', userController.login);
 router.get('/registro',userController.registro);
+router.post('/registro',upload.single("foto-perfil"),userController.createUser);
 
 module.exports=router;
